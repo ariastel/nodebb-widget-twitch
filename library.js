c07b85818fe1202cc76dbd95ee663c973a002680
Antosik <grig_anton96@mail.ru>
2021-02-13 22:40:38 +0300

    initial version

---
+++ library.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const nconf = require.main.require('nconf');
+const url = require('url');
+
+let app;
+const Widget = {};
+
+Widget.init = async function (params) {
+	app = params.app;
+};
+
+Widget.defineWidgets = async function (widgets) {
+	return widgets.concat([
+		{
+			widget: 'aa_twitch',
+			name: 'Twitch',
+			description: 'Displays a Twitch stream.',
+			content: await app.renderAsync('admin/twitch', {}),
+		}
+  ]);
+}
+
+Widget.renderTwitchWidget = async function (widget) {
+	if (!isVisibleInCategory(widget)) {
+		return null;
+	}
+	const data = {
+    channel_name: widget.data.channel_name,
+    parent_url: url.parse(nconf.get('url')).hostname,
+  };
+	widget.html = await app.renderAsync('widgets/twitch', data);
+	return widget;
+};
+
+module.exports = Widget;
+
+
+function getCidsArray(widget) {
+	const cids = widget.data.cid || '';
+	return cids.split(',').map(c => parseInt(c, 10)).filter(Boolean);
+}
+
+function isVisibleInCategory(widget) {
+	const cids = getCidsArray(widget);
+	return !(cids.length && (widget.templateData.template.category || widget.templateData.template.topic) && !cids.includes(parseInt(widget.templateData.cid, 10)));
+}
---
+++ templates/admin/twitch.tpl
@@ -0,0 +1,8 @@
+<label>
+	Category:<br />
+	<small>Set the category IDs you want to display this widget on (separated by commas)</small>
+</label>
+<input type="text" class="form-control" name="cid" placeholder="0" />
+
+<label>Channel name:</label>
+<input type="text" class="form-control" name="channel_name" placeholder="Channel name" />
